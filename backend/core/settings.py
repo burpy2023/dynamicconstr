6babe0f07245d5cb3fe033aeeba6c8f66643ab25
burpy2023 <marifwa10@gmail.com>
2025-05-23 15:22:43 -0500

    Revert "Update settings.py"
    This reverts commit 8b70ca26fe4f6c5d8f2117a9c7703ffbada05999.

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-CSRF_TRUSTED_ORIGINS = ['https://dynamicconstr-1.onrender.com']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
